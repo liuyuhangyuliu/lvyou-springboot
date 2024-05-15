86fe96b9aaa266235a8ca2911b347c85559e61de
liuyuhang <2196933343@qq.com>
2024-05-15 08:03:58 +0800

    intital project structure

---
+++ lvyou-web/src/main/java/edu/hit/lvyouweb/LvyouWebApplication.java
@@ -0,0 +1,13 @@
+package edu.hit.lvyouweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvyouWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LvyouWebApplication.class, args);
+    }
+
+}
---
+++ lvyou-web/src/test/java/edu/hit/lvyouweb/LvyouWebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hit.lvyouweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvyouWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
